b20b8c78cc2e938fd737b8429b5a5791fb379f24
Manukyan Sofia <manukian.st@phystech.edu>
2021-12-14 13:29:36 +0300

    грамматическая ошибочка

--- board.py
+++ board.py
@@ -17,16 +17,23 @@ class Board:
 
         """
         self.board = []
-        self.selcted_square = None
+        self.selected_square = None
         self.black_pieces = 12
         self.white_pieces = 12
         self.white_kings = 0
         self.black_kings = 0
 
     def draw_squares(self, screen):
+        
+        """ Функция рисует квадраты доски """
+        
         screen.fill(BROWN)
         for row in range(ROWS):
             for col in range(row % 2, ROWS, 2):
                 pygame.draw.rect(screen, BEIGE, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
                 
+    def new_board(self):
+        
+        """ Функция рисует шашки на доске в начале игры """
+
         
