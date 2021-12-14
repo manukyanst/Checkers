8334d96b79b7e9f950b86b89a8ebd22831d91236
Manukyan Sofia <manukian.st@phystech.edu>
2021-12-14 12:27:03 +0300

    главный модуль - запуск игры

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 import pygame
+from values import *
+from board import *
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Checkers')
+FPS = 60
+
+def main ():
+    finished = False
+    clock = pygame.time.Clock()
+    board = Board()
+    
+    while not finished:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+        board.draw_squares(screen)
+        pygame.display.update()
+
+    pygame.quit()
+
+main()
