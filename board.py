1316f72b61a5c984a8ce516b99b176c1e111ada1
mishasurmach <91309866+mishasurmach@users.noreply.github.com>
2021-12-14 20:25:20 +0300

    Добавил функцию рисования фигурок и доски

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 import pygame
 from values import *
+from piece import *
 
 " в этом модуле описывается класс Доска и функции, связанные с ней"
 
@@ -13,7 +14,7 @@ class Board:
         selected_square - значение, показывающее, выбран ли квадрат на доске для хода
         black/white pieces - количество белых или черных шашек
         black/white kings - количество белых или черных королей
-        
+        create_board - создание массива данных доски (строки и столбцы)
 
         """
         self.board = []
@@ -22,6 +23,7 @@ class Board:
         self.white_pieces = 12
         self.white_kings = 0
         self.black_kings = 0
+        self.create_board()
 
     def draw_squares(self, screen):
         
@@ -29,11 +31,37 @@ class Board:
         
         screen.fill(BROWN)
         for row in range(ROWS):
-            for col in range(row % 2, ROWS, 2):
+            for col in range(row % 2, COLS, 2):
                 pygame.draw.rect(screen, BEIGE, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
                 
     def new_board(self):
         
         """ Функция рисует шашки на доске в начале игры """
+        
+        for row in range(ROWS):
+            self.board.append([])
+            for col in range(COLS):
+                if col % 2 == ((row + 1) % 2):
+                    if raw < 3:
+                        self.board[row].append(Piece(row, col, WHITE))
+                    elif row > 4:
+                        self.board[row].append(Piece(row, col, RED))
+                    else:
+                        self.board[row].append(0)
+                else:
+                    self.board[row].append(0)
+               
+     def draw(self, screen):
+        
+        """ Функция рисует квадраты и шашки на экране """
+        
+        self.draw_squares(screen)
+        for row in range(ROWS):
+            for col in range(COLS):
+                piece = self.board[row][col]
+                if piece != 0:
+                    piece.draw(screen)
+
+
 
         
