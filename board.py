c966dd0dcc2e637301bebb5563759cf40d09d8bd
Manukyan Sofia <manukian.st@phystech.edu>
2021-12-14 12:26:38 +0300

    класс доска: начало

---
+++ board.py
@@ -0,0 +1,32 @@
+import pygame
+from values import *
+
+" в этом модуле описывается класс Доска и функции, связанные с ней"
+
+class Board:
+    def __init__(self):
+        """ Конструктор класса Board
+
+        Arguments:
+        board - двумерный массив, в котором хранится информация
+        о положении шашек на доске
+        selected_square - значение, показывающее, выбран ли квадрат на доске для хода
+        black/white pieces - количество белых или черных шашек
+        black/white kings - количество белых или черных королей
+        
+
+        """
+        self.board = []
+        self.selcted_square = None
+        self.black_pieces = 12
+        self.white_pieces = 12
+        self.white_kings = 0
+        self.black_kings = 0
+
+    def draw_squares(self, screen):
+        screen.fill(BROWN)
+        for row in range(ROWS):
+            for col in range(row % 2, ROWS, 2):
+                pygame.draw.rect(screen, BEIGE, (row*SQUARE_size, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                
+        
