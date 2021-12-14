d6e2f6a2ec6bde52fcd407b60f2061f1894194f4
mishasurmach <91309866+mishasurmach@users.noreply.github.com>
2021-12-14 20:29:11 +0300

    Хочу посмотреть результат

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main ():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pass
 
-        board.draw_squares(screen)
+        board.draw(screen)
         pygame.display.update()
 
     pygame.quit()
