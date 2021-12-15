ed5e033c8ad2e0e52d82d09e48bbf7aa19589100
mishasurmach <91309866+mishasurmach@users.noreply.github.com>
2021-12-15 14:34:24 +0300

    Добавил пару функций логистики
    связанные с движением шашек

--- piece.py
+++ piece.py
@@ -34,21 +34,36 @@ class Piece:
         self.calc_pos()
 
     def calc_pos(self):
+
         """ Функция определяет координаты центра шашки исходя из значений строки и столбца """
         
         self.x = SQUARE_SIZE * self.col + SQUARE_SIZE//2 
         self.y = SQUARE_SIZE * self.row + SQUARE_SIZE//2
 
     def make_king(self):
+
         """ Функция делает шашку королем """
+
         self.king = True
 
+    def move(self, row, col):
+        
+        """ Фунцкия двигает шашку и пересчитывает координаты её левого перхнего угла """
+
+        self.row = row
+        self.col = col
+        self.calc_pos()
+
+
     def draw(self, screen):
+
         """ Функция отрисовывает шашку в нужном положении """
+
         r = SQUARE_SIZE//2 - 5 * BOARDER
         pygame.draw.circle(screen, self.line_color, (self.x, self.y), r + LINE)
         pygame.draw.circle(screen, self.color, (self.x, self.y), r)
-        
+        if self.king:
+            screen.blit(CROWN, (self.x - CROWN.get_width() // 2, self.y - CROWN.get_height() // 2))
         
 
 
