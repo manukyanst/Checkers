a239e7f58326dce0d107eabec7187c837a81f189
NailKhas <91308380+NailKhas@users.noreply.github.com>
2021-12-16 17:07:08 +0300

    Update board.py

--- board.py
+++ board.py
@@ -1,13 +1,13 @@
 import pygame
 from values import *
-from piece import *
+from piece import Piece
 
 " в этом модуле описывается класс Доска и функции, связанные с ней"
+
 
 class Board:
     def __init__(self):
         """ Конструктор класса Board
-
         Arguments:
         board - двумерный массив, в котором хранится информация
         о положении шашек на доске
@@ -15,52 +15,47 @@ class Board:
         black/white pieces - количество белых или черных шашек
         black/white kings - количество белых или черных королей
         create_board - создание массива данных доски (строки и столбцы)
-
         """
         self.board = []
-        self.selected_square = None
-        self.black_pieces = 12
-        self.white_pieces = 12
-        self.white_kings = 0
-        self.black_kings = 0
+        self.black_pieces = self.white_pieces = 12
+        self.white_kings = self.black_kings = 0
         self.create_board()
 
     def draw_squares(self, screen):
-        
+
         """ Функция рисует квадраты доски """
-        
+
         screen.fill(BROWN)
         for row in range(ROWS):
             for col in range(row % 2, COLS, 2):
-                pygame.draw.rect(screen, BEIGE, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                pygame.draw.rect(screen, BEIGE, (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
     def move(self, piece, row, col):
 
         """ Функция меняет позицию шашки """
-        
+
         self.board[piece.row][piece.col], self.board[row][col] = self.board[row][col], self.board[piece.row][piece.col]
-        #меняем местами начальные "координаты" шашки с конечными в списке
+        # меняем местами начальные "координаты" шашки с конечными в списке
         piece.move(row, col)
 
-        #если шашка дошла до конца доски, она становимся королем
-        if row == ROWS or row == 0:
+        # если шашка дошла до конца доски, она становимся королем
+        if row == ROWS - 1 or row == 0:
             piece.make_king()
             if piece.color == WHITE:
                 self.white_kings += 1
             else:
                 self.black_kings += 1
-        
-    
+
     def get_piece(self, row, col):
-        
+
         """ Функция возвращает объект по номеру строки и столбца """
-        
+
         return self.board[row][col]
-    
+
     def create_board(self):
-        
+
         """ Функция рисует шашки на доске в начале игры """
-        
+
         for row in range(ROWS):
             self.board.append([])
             for col in range(COLS):
@@ -73,14 +68,119 @@ class Board:
                         self.board[row].append(0)
                 else:
                     self.board[row].append(0)
-               
+
     def draw(self, screen):
-        
+
         """ Функция рисует квадраты и шашки на экране """
-        
+
         self.draw_squares(screen)
         for row in range(ROWS):
             for col in range(COLS):
                 piece = self.board[row][col]
                 if piece != 0:
                     piece.draw(screen)
+
+    def remove(self, pieces):
+        for piece in pieces:
+            self.board[piece.row][piece.col] = 0
+            if piece != 0:
+                if piece.color == BROWN:
+                    self.black_pieces -= 1
+                else:
+                    self.white_pieces -= 1
+
+    def winner(self):
+        if self.black_pieces <= 0:
+            return WHITE
+        elif self.white_pieces <= 0:
+            return BROWN
+
+        return None
+
+    def get_valid_moves(self, piece):
+        moves = {}
+        left = piece.col - 1
+        right = piece.col + 1
+        row = piece.row
+
+        if piece.color == BROWN or piece.king:
+            moves.update(self._traverse_left(row - 1, max(row - 3, -1), -1, piece.color, left))
+            moves.update(self._traverse_right(row - 1, max(row - 3, -1), -1, piece.color, right))
+        if piece.color == WHITE or piece.king:
+            moves.update(self._traverse_left(row + 1, min(row + 3, ROWS), 1, piece.color, left))
+            moves.update(self._traverse_right(row + 1, min(row + 3, ROWS), 1, piece.color, right))
+
+        return moves
+
+    def _traverse_left(self, start, stop, step, color, left, skipped=None):
+        if skipped is None:
+            skipped = []
+        moves = {}
+        last = []
+        for r in range(start, stop, step):
+            if left < 0:
+                break
+
+            current = self.board[r][left]
+            if current == 0:
+                if skipped and not last:
+                    break
+                elif skipped:
+                    moves[(r, left)] = last + skipped
+                else:
+                    moves[(r, left)] = last
+
+                if last:
+                    if step == -1:
+                        row = max(r - 3, 0)
+                    else:
+                        row = min(r + 3, ROWS)
+                    moves.update(self._traverse_left(r + step, row, step, color, left - 1, skipped=last))
+                    moves.update(self._traverse_right(r + step, row, step, color, left + 1, skipped=last))
+                break
+            elif current.color == color:
+                break
+            else:
+                last = [current]
+
+            left -= 1
+
+        return moves
+
+    def _traverse_right(self, start, stop, step, color, right, skipped=None):
+        if skipped is None:
+            skipped = []
+        if skipped is None:
+            skipped = []
+        moves = {}
+        last = []
+        for r in range(start, stop, step):
+            if right >= COLS:
+                break
+
+            current = self.board[r][right]
+            if current == 0:
+                if skipped and not last:
+                    break
+                elif skipped:
+                    moves[(r, right)] = last + skipped
+                else:
+                    moves[(r, right)] = last
+
+                if last:
+                    if step == -1:
+                        row = max(r - 3, 0)
+                    else:
+                        row = min(r + 3, ROWS)
+                    moves.update(self._traverse_left(r + step, row, step, color, right - 1, skipped=last))
+                    moves.update(self._traverse_right(r + step, row, step, color, right + 1, skipped=last))
+                break
+            elif current.color == color:
+                break
+            else:
+                last = [current]
+
+            right += 1
+
+        return moves
+
