61d71b701eebc673f761b80c6f51edaf8d103a25
NailKhas <91308380+NailKhas@users.noreply.github.com>
2021-12-16 17:08:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,46 @@
 import pygame
 from values import *
-from board import *
-from piece import *
+from game import Game
+
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 FPS = 60
 
-def get_row_col_from_mouse(pos):
 
+def get_row_col_from_mouse(pos):
     """ Функция возвращает номер строки и столбца по положению мыши """
-    
+
     x, y = pos
     row = y // SQUARE_SIZE
     col = x // SQUARE_SIZE
     return row, col
-    
-def main ():
+
+
+def main():
     finished = False
     clock = pygame.time.Clock()
-    board = Board()
+    game = Game(screen)
 
-    
     while not finished:
         clock.tick(FPS)
+
+        if game.winner() is not None:
+            print(game.winner())
+            finished = True
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 finished = True
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
-                piece = board.get_piece(row, col)
-                board.move(piece, 4, 3)
+                game.select(row, col)
 
-        board.draw(screen)
-        pygame.display.update()
+        game.update()
 
     pygame.quit()
+
 
 main()
