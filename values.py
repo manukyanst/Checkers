2e2448defa0388a1c233b4880ca685cec575a565
Manukyan Sofia <manukian.st@phystech.edu>
2021-12-18 18:09:52 +0300

    зеленый

--- values.py
+++ values.py
@@ -3,17 +3,19 @@ import pygame
 " В этом файле хранятся все константы - значения размеров поля, картинки и т.д."
 
 WIDTH, HEIGHT = 800, 800
-ROWS, COLS = 8, 8
-SQUARE_SIZE = WIDTH // COLS
-LINE = 1
-BOARDER = 2
-# rgb
+ROWS, COLS = 8, 8 #количество строк и столбцов
+SQUARE_SIZE = WIDTH // COLS #размер квадрата доски
+LINE = 1 #ширина границы шашки
+BOARDER = 2 #отступ 
+
+# цвета rgb
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BROWN = (100, 67, 33)
 BLUE = (0, 0, 255)
 GREY = (128, 128, 128)
 BEIGE = (255, 230, 180)
+GREEN = (0, 255, 0)
 
 CROWN = pygame.transform.scale(pygame.image.load('graphics/crown.png'), (44, 25))
 
