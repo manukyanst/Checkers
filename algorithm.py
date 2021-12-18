818b548a1fb236cda0de75be41d00e9f3b4583db
Manukyan Sofia <manukian.st@phystech.edu>
2021-12-18 19:13:46 +0300

    пришлось добавить game в переменные

--- algorithm.py
+++ algorithm.py
@@ -3,7 +3,7 @@ import pygame
 from values import *
 
 
-def minimax(board, depth, max_player):
+def minimax(board, depth, max_player, game):
     """Функция осуществляет выбор наилучшего хода для ии
         board - позиция (вариант доски)
         depth - глубина (на сколько ходов вперед просчитывается)
@@ -16,8 +16,8 @@ def minimax(board, depth, max_player):
     if max_player:
         maxEval = float('-inf')
         best_move = None
-        for move in get_all_moves(board, WHITE):
-            evaluation = minimax(move, depth - 1, False)[0]
+        for move in get_all_moves(board, WHITE, game):
+            evaluation = minimax(move, depth - 1, False, game)[0]
             maxEval = max(maxEval, evaluation)
             if maxEval == evaluation:
                 best_move = move
@@ -26,8 +26,8 @@ def minimax(board, depth, max_player):
     else:
         minEval = float('+inf')
         best_move = None
-        for move in get_all_moves(board, BLACK):
-            evaluation = minimax(move, depth - 1, True)[0]
+        for move in get_all_moves(board, BLACK, game):
+            evaluation = minimax(move, depth - 1, True, game)[0]
             minEval = min(minEval, evaluation)
             if minEval == evaluation:
                 best_move = move
@@ -35,7 +35,7 @@ def minimax(board, depth, max_player):
         return minEval, best_move
 
 
-def simulate_move(piece, move, board, skip):
+def simulate_move(piece, move, board, game, skip):
     """Функция создает и возвращает версию доски с предполагаемым ходом"""
     board.move(piece, move[0], move[1])
     if skip:
@@ -44,18 +44,25 @@ def simulate_move(piece, move, board, skip):
     return board
 
 
-def get_all_moves(board, color):
+def get_all_moves(board, color, game):
     """Функция возвращает все возможные варианты ходов(доски) с данной позиции"""
     moves = []
 
     for piece in board.get_all_pieces(color):
         valid_moves = board.get_valid_moves(piece)
         for move, skip in valid_moves.items():
+            draw_moves(game, board, piece)
             temp_board = deepcopy(board)
             temp_piece = temp_board.get_piece(piece.row, piece.col)
-            new_board = simulate_move(temp_piece, move, temp_board, skip)
+            new_board = simulate_move(temp_piece, move, temp_board, game, skip)
             moves.append(new_board)
     
     return moves
 
-
+def draw_moves(game, board, piece):
+    """Функция отрисовывает выбранный ход и доску"""
+    valid_moves = board.get_valid_moves(piece)
+    board.draw(game.screen)
+    pygame.draw.circle(game.screen, GREEN, (piece.x, piece.y), 50, 5)
+    game.draw_valid_moves(valid_moves.keys())
+    pygame.display.update()
