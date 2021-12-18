51d31d8e508c2bdea136c2cc5ff77ab876c2916f
NailKhas <91308380+NailKhas@users.noreply.github.com>
2021-12-18 12:47:02 +0300

    Добавил документацию

--- piece.py
+++ piece.py
@@ -1,10 +1,13 @@
 from values import *
 import pygame
 
+" В этом модуле описывается класс Шашки и функции, связанные с шашками "
+
 
 class Piece:
 
     def __init__(self, row, col, color):
+        """Конструктор класса Piece - шашка"""
         self.row = row
         self.col = col
         self.color = color
@@ -14,20 +17,24 @@ class Piece:
         self.calc_pos()
 
     def calc_pos(self):
+        """ Функция определяет координаты центра шашки исходя из значений строки и столбца """
         self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
         self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
 
     def make_king(self):
+        """ Функция делает шашку королем """
         self.king = True
 
     def draw(self, screen):
-        r = SQUARE_SIZE // 2 - 5*self.BOARDER
+        """ Функция отрисовывает шашку в нужном положении """
+        r = SQUARE_SIZE // 2 - 5*BOARDER
         pygame.draw.circle(screen, GREY, (self.x, self.y), r + LINE)
         pygame.draw.circle(screen, self.color, (self.x, self.y), r)
         if self.king:
             screen.blit(CROWN, (self.x - CROWN.get_width() // 2, self.y - CROWN.get_height() // 2))
 
     def move(self, row, col):
+        """ Фунцкия двигает шашку и пересчитывает координаты её левого перхнего угла """
         self.row = row
         self.col = col
         self.calc_pos()
