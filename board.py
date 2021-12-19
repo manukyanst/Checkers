a026fc3937015c00afa82bfb4107e801ba97c63e
Manukyan Sofia <manukian.st@phystech.edu>
2021-12-19 20:42:27 +0300

    может есть несколько раз теперь

--- board.py
+++ board.py
@@ -149,7 +149,7 @@ class Board:
 
                 if last:
                     if step == -1:
-                        row = max(r - 3, 0)
+                        row = max(r - 3, -1)
                     else:
                         row = min(r + 3, ROWS)
                     moves.update(self._traverse_left(r + step, row, step, color, left - 1, skipped=last))
@@ -186,7 +186,7 @@ class Board:
 
                 if last:
                     if step == -1:
-                        row = max(r - 3, 0)
+                        row = max(r - 3, -1)
                     else:
                         row = min(r + 3, ROWS)
                     moves.update(self._traverse_left(r + step, row, step, color, right - 1, skipped=last))
