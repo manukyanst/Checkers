6dcf8a03242b5d85644a62b8776798600b552236
mishasurmach <91309866+mishasurmach@users.noreply.github.com>
2021-12-20 19:07:09 +0300

    главный файл с меню

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import pygame
 from values import *
 from game import Game
 from algorithm import minimax
+from menu import *
+
+
+pygame.init()
+f1 = pygame.font.Font(None, 36)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 FPS = 60
+game_mode = 0
+
 
 
 def get_row_col_from_mouse(pos):
@@ -17,34 +24,46 @@ def get_row_col_from_mouse(pos):
     return row, col
 
 
-def main():
+def main(game_mode):
     finished = False
     clock = pygame.time.Clock()
     game = Game(screen)
 
     while not finished:
-        clock.tick(FPS)
-
-        if game.turn == WHITE:
-            value, new_board = minimax(game.get_board(), 4, WHITE, game)
-            game.ai_move(new_board)
-
-        if game.winner() is not None:
-            print(game.winner())
-            finished = True
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 finished = True
 
             if event.type == pygame.MOUSEBUTTONDOWN:
+                game_mode = start_game(event.pos[0], event.pos[1], game_mode, screen)
+
+        if game_mode == 0:
+            draw_menu(screen)
+            
+        if game_mode == 1:
+
+            if game.turn == WHITE:
+                value, new_board = minimax(game.get_board(), 4, WHITE, game)
+                game.ai_move(new_board)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
                 game.select(row, col)
+            game.update()
 
-        game.update()
+        if game_mode == 2:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
+            game.update()
 
+        if game.winner() is not None:
+            print(game.winner())
+            finished = True
+     
     pygame.quit()
 
-
-main()
+main(game_mode)
