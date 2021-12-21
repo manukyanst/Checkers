89eaf109bb597a877d70c508293f48232f3da105
Manukyan Sofia <manukian.st@phystech.edu>
2021-12-21 11:22:09 +0300

    добавлена функция для победителя

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ from values import *
 
 pygame.init()
 f1 = pygame.font.Font(None, 36)
+f2 = pygame.font.Font(None, 100)
 
 def draw_menu(screen):
     ''' Функция рисует меню на экране '''
@@ -19,11 +20,21 @@ def draw_menu(screen):
 def start_game(x, y, game_mode, screen):
     ''' Функция воспринимает нажатия на конкретный пункт меню '''
     if game_mode == 0:
-        
         if (x > WIDTH // 8 and x < WIDTH // 8 + WIDTH * 2 // 3 + 50 and y > HEIGHT // 3 and y < HEIGHT // 3 + HEIGHT // 8):
             game_mode = 1
-
         if (x > WIDTH // 8 and x <  WIDTH // 8 + WIDTH * 2 // 3 + 50 and y > HEIGHT // 2 and y < HEIGHT // 2 + HEIGHT // 3 + HEIGHT // 8 + HEIGHT // 8):
             game_mode = 2
-
     return game_mode
+
+def end_game(winner, screen):
+    """Функция показывает на экране, кто победитель"""
+    text_black = f2.render('BLACK WINS', True, WHITE)
+    text_white = f2.render('WHITE WINS', True, BLACK)
+    if winner == BLACK:
+        screen.fill(BLACK)
+        screen.blit(text_black, (200, 350))
+    else:
+        screen.fill(WHITE)
+        screen.blit(text_white, (200, 350))
+    pygame.display.update()
+        
